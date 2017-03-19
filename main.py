48650b8ecfb8259a870a8c82c5c23db4a530a019
Francisco <kener18@gmail.com>
2017-03-19 21:39:42 +0100

    probando..

--- main.py
+++ main.py
@@ -4,11 +4,11 @@
 ## Extractor de eventos v0.00001
 ## Autores: 
 ##	Jean-Francois Kener
-##	Kato Spiegel
+##	Carlos Vivar
 ##
 
 import sys, os, time, json
-from modules import twitter
+from modules import twitter, regex_data
 import feedparser
 
 debug = 1
@@ -31,8 +31,9 @@ if len(sys.argv) > 1:
 	url = sys.argv[1]
 
 
-# Twitter
+######################################################## Twitter
 
+## Connect and get
 twitter.Auth()
 
 twits = []
@@ -42,6 +43,15 @@ for tw in config['twitter_accounts']:
 		twits.extend(add_twits)
 print 'Total: ' + str(len(twits))
 
+
+## Extract infos (regex)
+for tw in twits:
+	meta = regex_data.RegexExtractData(tw['text'])
+	for attr in meta:
+		tw[attr] = meta[attr]
+
+
+## Dump
 if debug:
 	f = 'debug/debug_twits.txt'
 	print "Dumping file " + f
@@ -51,28 +61,42 @@ if debug:
 			obj.write(descriptor + ": " + item[descriptor].encode('utf-8') + "\n")
 		obj.write("\n\n")	
 	obj.close
+	print "Ready\n\n\n"
 
 
+######################################################## Feeds
 
-# Feeds
+entries = []
 
+## Connect and get
 for feed in config['feeds']:
 	url = feed['link']
 	d = feedparser.parse( url )
+	entries.extend(d.entries)
 	print 'Extracted feed: ' + d.feed.title
 	print 'Total: ' + str(len(d.entries))
+	print "\n"
 
+## Extract data (regex)
+for item in entries:
+		meta[item] = regex_data.RegexExtractData(item.description)
+
+
+## Dump
 if debug:
 	f = 'debug/debug_feeds.txt'
 	print "Dumping file " + f
 	obj = open(f, 'wb')
 
-	for item in d.entries:
+
+	for item in entries:
 		obj.write(item.title.encode('utf-8') + "\n")
 		obj.write(item.published.encode('utf-8') + "\n")	
 		obj.write(item.description.encode('utf-8') + "\n")
 		obj.write(item.link.encode('utf-8') + "\n")
+		for descriptor in meta[item]:
+			obj.write(descriptor + ": " + meta[item][descriptor].encode('utf-8') + "\n")
 		obj.write("\n\n")
 	obj.close
-
+	print "Ready\n\n\n"
 
